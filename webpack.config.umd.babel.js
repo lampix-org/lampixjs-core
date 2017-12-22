1e5d096ef259596b95a1e68f70e4c73951accd8c
Dan Balaban <dan.stefan.balaban@gmail.com>
2017-12-22 14:00:29 -0500

    Replaced generic webpack config with UMD specific one

---
+++ webpack.config.umd.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default (env = {}) => ({
+  entry: {
+    'lampix': './src/index.ts'
+  },
+  output: {
+    filename: 'lampix.js',
+    path: path.resolve(__dirname, 'lib/umd'),
+    library: 'lampix',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".ts", ".js"],
+    modules: [
+      'src',
+      'node_modules'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+});
