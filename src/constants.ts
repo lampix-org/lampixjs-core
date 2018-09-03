8de57b32cc835f41b5abdcc0c5d1aa40e23ca7cf
Dan Balaban <dbalaban@lampix.co>
2018-09-03 16:05:58 +0300

    WatcherTypes enum with classifier and segmenter

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,8 @@ export const APP_SWITCHER_NAME = 'App Switcher';
 
 export const WINDOW_WIDTH = 1280;
 export const WINDOW_HEIGHT = 800;
+
+export enum WatcherTypes {
+  Classifier = 'classifier',
+  Segmenter = 'segmenter'
+}
