6813ba348dc69f2b88f755294f85129e7546fc8b
Dan Balaban <dbalaban@lampix.co>
2018-09-04 11:09:59 +0300

    Updated public API with watchers instead of simple / position

--- src/LampixBridge.ts
+++ src/LampixBridge.ts
@@ -1,8 +1,12 @@
-import { _getLampixInfo } from './api/getLampixInfo';
+import { getLampixInfo } from './api/getLampixInfo';
+import { createRegisteredWatcher } from './api/createRegisteredWatcher';
+
+import * as WatcherManager from './adapters/watcher-registration/manager';
 
 import {
   ILampixBridge,
-  LampixInfo
+  LampixInfo,
+  PublicAPI
 } from './types';
 
 const internalLampixAPI = window._lampix_internal;
@@ -16,9 +20,33 @@ class LampixBridge implements ILampixBridge {
    */
   public getLampixInfo(): Promise<LampixInfo> {
     return new Promise((resolve) => {
-      _getLampixInfo(internalLampixAPI, resolve);
+      getLampixInfo(internalLampixAPI, resolve);
     });
   }
+
+  /**
+   * Watcher manager
+   * - facilitates adding watchers
+   * - facilitates removing multiple watchers with one call
+   *
+   * Alternatively, removing is done individually via
+   * {@link RegisteredWatcher.remove | watcher's remove() function}
+   */
+  public watchers: PublicAPI.WatcherRegistrar = {
+    add(...rectangles) {
+      const watchers = rectangles.map(createRegisteredWatcher);
+      WatcherManager.addWatchers(watchers);
+
+      // Impose async to allow the development of proper communication between
+      // device API and JS SDK without prompting a major semver bump in the near future
+      return Promise.resolve(watchers);
+    },
+    remove(...registeredWatchers) {
+      console.log(registeredWatchers);
+
+      return Promise.resolve();
+    }
+  };
 }
 
 export { LampixBridge };
