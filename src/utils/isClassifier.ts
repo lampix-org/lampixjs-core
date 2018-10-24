8beb6235ff574de9bb362834a5c32ab3e4c2214b
Dan Balaban <dbalaban@lampix.co>
2018-10-24 10:19:31 +0300

    Removed isClassifier / isSegmenter utils

--- src/utils/isClassifier.ts
+++
@@ -1,5 +0,0 @@
-import { RegisteredWatcher, Watcher } from '../types';
-
-const isClassifier = (w: RegisteredWatcher) => w.source.type === Watcher.Types.Classifier;
-
-export { isClassifier };
--- src/utils/isSegmenter.ts
+++
@@ -1,5 +0,0 @@
-import { RegisteredWatcher, Watcher } from '../types';
-
-const isSegmenter = (w: RegisteredWatcher) => w.source.type === Watcher.Types.Segmenter;
-
-export { isSegmenter };
