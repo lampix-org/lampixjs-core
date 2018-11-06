7ca4ae9d40cfd792b4fb75e5a1065c1634c74d06
Dan Balaban <dbalaban@lampix.co>
2018-11-06 13:06:11 +0200

    Use fetch to retrieve config.json

--- src/api/getAppConfig.ts
+++ src/api/getAppConfig.ts
@@ -1,19 +1,26 @@
-import { waitForAPI } from './waitForAPI';
-import { LampixEvents } from '../events';
-import { listen } from './managers/communication/settler';
-
 /**
  * Business logic for retrieving data held in config.json (if available)
+ * No point in involving the simulator or the device in this
  *
  * @internal
  */
 const getAppConfig = () => (): Promise<object> =>
-  waitForAPI()
-    .then(() => {
-      const promise = listen(LampixEvents.AppConfig);
-      window._lampix_internal.get_config_data();
-      return promise;
+  fetch('./config.json')
+    .then((r) => {
+      if (!r.ok) {
+        return null;
+      }
+
+      return r.text();
     })
-    .then(({ config }) => config);
+    .then((body) => {
+      if (!body) return null;
+
+      try {
+        return JSON.parse(body);
+      } catch (e) {
+        return null;
+      }
+    });
 
 export { getAppConfig };
