161cc05d33c3289a646bad21c3306c50d744b1a5
Dan Balaban <dbalaban@lampix.co>
2018-11-12 13:24:27 +0200

    Added types for generateId params

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,8 +1,8 @@
 // dec2hex :: Integer -> String
-const dec2hex = (dec) => ('0' + dec.toString(16)).substr(-2);
+const dec2hex = (dec: number) => ('0' + dec.toString(16)).substr(-2);
 
 // generateId :: Integer -> String
-const generateId = (len = 40) => {
+const generateId = (len: number = 40) => {
   const arr = new Uint8Array(len / 2);
   window.crypto.getRandomValues(arr);
   return Array.from(arr, dec2hex).join('');
